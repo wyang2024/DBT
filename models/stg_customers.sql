0f852ca00e1c3c2a184e1f59ab55637b82104e8e
wyang97@yahoo.com <wyang97@yahoo.com>
2024-06-02 15:28:10 +0000

    12

--- models/stg_customers.sql
+++ models/stg_customers.sql
@@ -1,3 +1,11 @@
+{{
+    config(
+        materialized='table',
+        pre_hook='delete from ANALYTICS.DBT_WYANG.TEMP_CUSTOMERS',
+        post_hook='create table ANALYTICS.DBT_WYANG.TEMP_ORDERS as select * from RAW.JAFFLE_SHOP.ORDERS'
+    )
+}}
+
 select
     id as customer_id,
     first_name,
